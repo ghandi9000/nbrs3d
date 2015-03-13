1b5f4c7bf1e18e2abddf1dcd1bd1551e397da7cd
Noah Peart <noah.v.peart@gmail.com>
2015-03-12 21:04:26 -0400

    Batch update

--- normal-ellipses.R
+++ normal-ellipses.R
@@ -3,7 +3,7 @@
 ## Description: Ellipse formed on normal plane to point of view intersection with ellipsoid
 ## Author: Noah Peart
 ## Created: Thu Feb 19 17:51:22 2015 (-0500)
-## Last-Updated: Fri Feb 20 23:59:13 2015 (-0500)
+## Last-Updated: Thu Mar 12 15:59:57 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 source("~/work/nbrs3d/vis.R")  # 3d visualization stuff (p3d, rgl shapes)
@@ -126,11 +126,11 @@ ell_tang2d <- function(a, b, xc, yc, theta) {
 ## Oblate ellipsoid centered (1, 1, 1), point of view from origin
 ## normal plane is simply <1,1,1>
 ## axes: a = b = 1, c = 2
-a <- 3
+a <- 1
 b <- 1
-c <- 2
-xc <- 1
-yc <- 2
+c <- 3
+xc <- 4
+yc <- 3
 zc <- 1
 
 open3d()
@@ -138,7 +138,11 @@ plot3d(0, xlab = "X", ylab = "Y", zlab = "Z",
        xlim = c(-3, 5), ylim = c(-3, 5), zlim = c(-3, 5))
 grid3d(side = c("x-","y+","z-"))
 abclines3d(0, a=diag(3), lwd=2)
-wire3d(ellipsoid(1,1,1,1,1,2), alpha = 1, col = "darkred")
+wire3d(ellipsoid(xc, yc, zc, a, b, c), alpha = 1, col = "darkred")
+
+## Add unit sphere
+.sphere <- ellipsoid3d(qmesh = T, trans = diag(4))
+wire3d(.sphere, col='blue', alpha=0.6)
 
 ## Add normal line and plane
 lines3d(x=c(0,1), y=c(0,1), z=c(0,1), col = "green", lwd=2)
